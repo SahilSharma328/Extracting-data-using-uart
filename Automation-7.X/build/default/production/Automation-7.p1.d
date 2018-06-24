ab3828e339393713cef9c8e818077356ec04cf0c
Sahil Sharma <31406807+SahilSharma328@users.noreply.github.com>
2018-06-24 11:17:24 +0530

    Add files via upload

---
+++ Automation-7.X/build/default/production/Automation-7.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/Automation-7.d  \
+ build/default/production/Automation-7.p1:  \
+ Automation-7.c 
---
+++ Automation-7.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Automation-7.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Automation-7.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=automation-7.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/automation-7.x.tar
